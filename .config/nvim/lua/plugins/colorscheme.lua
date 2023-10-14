c957f14608b3eabc2d521ce3081fe4d6a9f92457
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-10-14 22:11:51 +0700

    Use only rose pine

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,10 @@
 return {
     {
-        "nyoom-engineering/oxocarbon.nvim",
-        priority = 1000,
-        config = function()
-            api.nvim_create_user_command("Oxocarbon", function()
-                vim.cmd.colorscheme("oxocarbon")
-            end, {})
-        end
-    },
-
-    {
         "rose-pine/neovim",
         priority = 1000,
         name = "rose-pine",
         config = function()
-            api.nvim_create_user_command("RosePine", function()
                 vim.cmd.colorscheme("rose-pine")
-            end, {})
         end
-    },
-
-    {
-        "arcticicestudio/nord-vim",
-        priority = 1000,
-        config = function()
-            g.nord_uniform_diff_background = 1
-            g.nord_italic = 1
-            g.nord_italic_comments = 1
-            g.nord_underline = 1
-
-            api.nvim_create_user_command("Nord", function()
-                vim.cmd.colorscheme("nord")
-            end, {})
-
-            vim.cmd("Nord")
-        end,
     },
 }
