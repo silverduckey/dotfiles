5e1e5789cdd205f74fd9098d1f12efabb1a415e2
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-10-26 20:21:18 +0700

    Disable firefox view

--- .mozilla/firefox/vu/user.js
+++ .mozilla/firefox/vu/user.js
@@ -95,6 +95,7 @@ user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 user_pref("accessibility.force_disabled", 1);
 user_pref("identity.fxaccounts.enabled", true);
 user_pref("browser.tabs.firefox-view", false);
+user_pref("browser.tabs.firefox-view-next", false);
 user_pref("permissions.default.desktop-notification", 2);
 user_pref("permissions.default.geo", 2);
 user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
