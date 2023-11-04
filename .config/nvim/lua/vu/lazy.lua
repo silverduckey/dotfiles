36adcad5d4c5e972db33750789250da4ab3f8dd7
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-11-04 21:07:50 +0700

    Fix formatting

--- .config/nvim/lua/vu/lazy.lua
+++ .config/nvim/lua/vu/lazy.lua
@@ -1,15 +1,15 @@
 local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 opt.rtp:prepend(lazypath)
 require("lazy").setup({
-    {import = "plugins" }
+    { import = "plugins" }
 })
