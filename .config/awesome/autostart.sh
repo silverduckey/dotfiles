fb479cfe2474c7806bf9de393e7c30fa8f5d4bf7
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2024-04-18 11:31:25 +0700

    Getrid of alacritty and rofi config

--- .config/awesome/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-killall picom
-killall lxsession
-killall ibus-daemon
-. $HOME/.screenlayout/screenlayout.sh
-feh --bg-fill --randomize $HOME/.wallpapers &
-picom &
-lxsession &
-ibus start
